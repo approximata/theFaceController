2162d408bb59f8ad0272ebb3d5f32777056b35d2
Mate Bodor <bodormate@gmail.com>
2017-06-17 19:37:44 +0200

    ignore server building

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const fs = require('fs-extra')
 const browserify = require('browserify')
 const port = process.env.PORT || 3000
 
-browserify(['client/src/main.js'])
-  .transform('babelify', {presets: ['es2015']})
-  .bundle()
-  .pipe(fs.createWriteStream('client/src/build/bundle.js'))
+// browserify(['client/src/main.js'])
+//   .transform('babelify', {presets: ['es2015']})
+//   .bundle()
+//   .pipe(fs.createWriteStream('client/src/build/bundle.js'))
 
 const app = express()
 
